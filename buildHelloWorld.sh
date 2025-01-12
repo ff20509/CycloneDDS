613f43f0368ba33f8e30e4dc3ae93b7e1b561f25
monkeyk <10569950+monkeyk@user.noreply.gitee.com>
2025-01-12 21:12:32 +0800

    remove something

--- buildHelloWorld.sh
+++
@@ -1,6 +0,0 @@
-cd examples\helloworld
-mkdir build
-# -G 指定生成器 -A 指定32位 or 64位（与编译库一致） -DCMAKE_PREFIX_PATH指向生成库位置
-cmake -G "Visual Studio 17 2022" -A Win32 -DCMAKE_PREFIX_PATH=D:\项目\Simulater_FreeRTOS_CycloneDDS\ddslib ..
-# 根据 Release 配置构建项目生成可执行文件
-cmake --build . --config Release
